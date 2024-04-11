e12df5dccb2fac4859f442fe8b41efe9f6e9c322
HollowReaper77 <dtomibite@gmail.com>
2024-04-11 23:58:00 +0200

    Playerscript has been added
    CDC-8

--- Challenger of DEMOnic City/Assets/Scripts/PlayerController.cs
+++ Challenger of DEMOnic City/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public int playerSpeed = 50;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
     }
 }
