176b53d551769a21e8234ab459ea85b683991251
HollowReaper77 <dtomibite@gmail.com>
2024-04-15 00:58:31 +0200

    Checkpoint Scripts
    CDC-9

---
+++ Challenger of DEMOnic City/Assets/Scripts/TrackCheckpoints.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCheckpoints : MonoBehaviour
+{
+
+    public event EventHandler OnPlayerCorrectCheckpoint;
+    public event EventHandler OnPlayerWrongCheckpoint;
+    private List<CheckpointSingle> checkpointSingleList;
+    private int nextCheckpointSingleIndex;
+
+    private void Awake()
+    {
+        Transform checkpointsTransform = transform.Find("Checkpoints");
+
+
+        foreach (Transform checkpointSingleTransoform in checkpointsTransform)
+        {
+            CheckpointSingle checkpointSingle = checkpointSingleTransoform.GetComponent<CheckpointSingle>();
+            checkpointSingle.SetTrackCheckpoints(this);
+
+            checkpointSingleList.Add(checkpointSingle);
+        }
+
+        nextCheckpointSingleIndex = 0;
+    }
+
+    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
+    {
+        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex)
+        {
+            nextCheckpointSingleIndex = (nextCheckpointSingleIndex+1) % checkpointSingleList.Count ;
+
+            OnPlayerCorrectCheckpoint?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            OnPlayerWrongCheckpoint?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
