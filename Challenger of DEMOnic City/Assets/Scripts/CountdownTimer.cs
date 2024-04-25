ea45a606dcfccc6aa31ccb32b3e360c254d0b78b
HollowReaper77 <dtomibite@gmail.com>
2024-04-25 11:35:27 +0200

    Countdown advancement
    CDC-13

---
+++ Challenger of DEMOnic City/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using NUnit.Framework.Constraints;
+using System;
+public class CountdownTimer : MonoBehaviour
+{
+    
+    float currentTime = 0f;
+    float startingTime = 5f;
+
+    [SerializeField] TextMeshProUGUI coundownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        coundownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 1 && currentTime >= 0)
+        {
+            coundownText.text = "GO!";
+        }
+
+        if (currentTime <= 0)
+        {
+            //currentTime = 0;
+
+            coundownText.enabled = false;
+        }
+    }
+
+    
+
+
+    /*
+    public int countdownTime;
+    public TextMeshProUGUI countdownDisplay;
+
+    private void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countdownTime--;
+        }
+
+        countdownDisplay.text = "GO";
+
+        yield return new WaitForSeconds(1f);
+
+        countdownDisplay.gameObject.SetActive(false);
+    }
+    */
+}
